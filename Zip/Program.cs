0931becb5d0ff7b1a6315b72c749b6fe8f187c37
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-03 05:47:24 +0200

    Zip

---
+++ Zip/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Zip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] colorNames = { "Red", "Green", "Blue" };
+            string[] colorNumbers = { "FFF", "EEE", "NNN" };
+
+            var colors = colorNames.Zip(colorNumbers, (name, hex) => $"{name} ({hex})");
+
+            foreach (var color in colors)
+            {
+                Console.WriteLine(color);
+            }
+        }
+    }
+}
