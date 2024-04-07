5eaa0dddfa9306d5f30cc08b42518541f5e12387
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-07 23:56:53 +0200

    Add ThenBy

---
+++ ThenBy/Program.cs
@@ -0,0 +1,18 @@
+﻿using LINQ.Shared;
+
+namespace ThenBy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = RepositoryV2.LoadEmployees();
+
+            var sortedEmpThenAsc = employees.OrderBy(e => e.Name).ThenBy(e => e.Salary);
+            var sortedEmpThenDesc = employees.OrderBy(e => e.Name).ThenByDescending(e => e.Salary);
+
+            sortedEmpThenAsc.Print("sortedEmpThenAsc");
+            sortedEmpThenDesc.Print("sortedEmpThenDesc");
+        }
+    }
+}
