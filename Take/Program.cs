73cc9178e30e9fe02ff5a66181d2c6623790c646
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-08 00:47:45 +0200

    Take

---
+++ Take/Program.cs
@@ -0,0 +1,21 @@
+﻿using LINQ.Shared.V3;
+
+namespace Take
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = Repository.LoadEmployees();
+
+            var emps = employees.Take(10);
+            emps.Print("Take First 10 employees");
+
+            var emps02 = employees.TakeWhile(e => e.Salary != 214400);
+            emps02.Print("Take while salary != 214400");
+
+            var emp03 = employees.TakeLast(10);
+            emp03.Print("Take Last 10 elements.");
+        }
+    }
+}
