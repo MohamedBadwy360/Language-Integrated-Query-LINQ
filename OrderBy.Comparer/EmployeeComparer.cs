1c3fefa70bfad43ebc37aae0677be3f9788c9d6b
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-07 23:56:24 +0200

    OrderBy.Comparer

---
+++ OrderBy.Comparer/EmployeeComparer.cs
@@ -0,0 +1,31 @@
+﻿using LINQ.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderBy.Comparer
+{
+    internal class EmployeeComparer : IComparer<EmployeeV2>
+    {
+        public int Compare(EmployeeV2? e1, EmployeeV2? e2)
+        {
+            // "2017-FI-1343"
+
+            int e1Year = Convert.ToInt32(e1?.EmployeeNo.Split("-")[0]);
+            int e2Year = Convert.ToInt32(e2?.EmployeeNo.Split("-")[0]);
+            int e1Seq = Convert.ToInt32(e1?.EmployeeNo.Split("-")[2]);
+            int e2Seq = Convert.ToInt32(e2?.EmployeeNo.Split("-")[2]);
+
+            if (e1Year == e2Year)
+            {
+                return e1Seq.CompareTo(e2Seq);
+            }
+            else
+            {
+                return e1Year.CompareTo(e2Year);
+            }
+        }
+    }
+}
---
+++ OrderBy.Comparer/Program.cs
@@ -0,0 +1,15 @@
+﻿using LINQ.Shared;
+
+namespace OrderBy.Comparer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<EmployeeV2> employees = RepositoryV2.LoadEmployees();
+            IOrderedEnumerable<EmployeeV2> sortedEmps = employees.OrderBy(e => e, new EmployeeComparer());
+
+            sortedEmps.Print("sortedEmps");
+        }
+    }
+}
