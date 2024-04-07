aa75fbdaa6daa24a7150247986781c7110cebe0a
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-08 00:47:28 +0200

    Add Skip

---
+++ Skip/Program.cs
@@ -0,0 +1,22 @@
+﻿using LINQ.Shared.V3;
+
+namespace Skip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = Repository.LoadEmployees();
+
+            var emps = employees.Skip(10);
+            emps.Print("Skip First 10 employees");
+
+            var emps02 = employees.SkipWhile(e => e.Salary != 214400);
+            emps02.Print("Skip while salary != 214400");
+
+            var emp03 = employees.SkipLast(10);
+            emp03.Print("Skip Last 10 elements.");
+
+        }
+    }
+}
