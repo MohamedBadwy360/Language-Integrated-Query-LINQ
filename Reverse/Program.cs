d83f4c22f2d0e308b545850f5873ef1c74317a50
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-07 23:56:36 +0200

    Reverse

---
+++ Reverse/Program.cs
@@ -0,0 +1,16 @@
+﻿using LINQ.Shared;
+
+namespace Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fruits = { "apricote", "banana", "orange", "mango", "apple", "grape", "strewberry" };
+
+            var fruitsReversed = fruits.Reverse();
+
+            fruitsReversed.Print("fruitsReversed");
+        }
+    }
+}
