0898b89d5b16903a3a0a09163b46de5d06b379fb
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-08 00:56:12 +0200

    Chunck

---
+++ Chunck/Program.cs
@@ -0,0 +1,19 @@
+﻿using LINQ.Shared.V3;
+
+namespace Chunck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = Repository.LoadEmployees();
+
+            var chuncks = employees.Chunk(10).ToList();
+
+            for (int i = 0; i < chuncks.Count; i++)
+            {
+                chuncks[i].Print($"Chunck[{i + 1}]");
+            }
+        }
+    }
+}
