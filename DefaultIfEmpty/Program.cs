2421197f65ad5e33385df002b083750dd808a725
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-12 06:39:54 +0200

    DefaultIfEmpty

---
+++ DefaultIfEmpty/Program.cs
@@ -0,0 +1,15 @@
+﻿using LINQ.Shared.V6;
+
+namespace DefaultIfEmpty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var questions = Enumerable.Empty<Question>();
+            var question = questions.DefaultIfEmpty(Question.Default);
+
+            question.ToQuiz();
+        }
+    }
+}
