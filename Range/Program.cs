d0c51dc8a2dc4f16ffeca280bae579dc14550133
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-12 06:48:32 +0200

    Range

---
+++ Range/Program.cs
@@ -0,0 +1,20 @@
+﻿using LINQ.Shared.V6;
+
+namespace Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var range = Enumerable.Range(1, 10);
+
+            foreach (var i in range)
+            {
+                Console.Write($" {i}");
+            }
+
+            var questions = QuestionBank.GetQuestionRange(range);
+            questions.ToQuiz();
+        }
+    }
+}
