f0532c28c63d1a485bd270827225cd372c33c440
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-12 06:55:34 +0200

    Repeat

---
+++ Repeat/Program.cs
@@ -0,0 +1,15 @@
+﻿using LINQ.Shared.V6;
+
+namespace Repeat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var q = QuestionBank.PickOne();
+
+            var questions = Enumerable.Repeat(q, 10);
+            questions.ToQuiz();
+        }
+    }
+}
