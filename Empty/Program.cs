65a0713b55686842c2b3eeaf644896693a46c5bb
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-12 06:26:45 +0200

    Empty

---
+++ Empty/Program.cs
@@ -0,0 +1,30 @@
+﻿using LINQ.Shared.V6;
+
+namespace Empty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // immediate execution
+            var questions = new List<Question>();
+            // 1
+            // .....
+            // 1000
+            foreach (var q in questions)
+            {
+                Console.WriteLine(q);
+            }
+
+            // deffered execution
+            var questions2 = Enumerable.Empty<Question>();
+            // 1
+            // .....
+            // 1000
+            foreach (var q in questions)
+            {
+                Console.WriteLine(q);
+            }
+        }
+    }
+}
