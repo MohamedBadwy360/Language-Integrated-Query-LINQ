73123ae9cd626e0a1072934bfa6423963a878184
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-14 01:42:23 +0200

    Last

---
+++ Last/Program.cs
@@ -0,0 +1,24 @@
+﻿using LINQ.Shared.V6;
+
+namespace Last
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var questions = QuestionBank.All;
+
+            var lastQuestion = questions.Last();
+
+            //var someQuestion = questions.Last(x => x.Title.Length == 0);   //InvalidOperationException
+            var someQuestion = questions.LastOrDefault(x => x.Title.Length == 0);
+
+            if (someQuestion is null)
+            {
+                Console.WriteLine("someQuestion is null.\n");
+            }
+
+            Console.WriteLine(lastQuestion);
+        }
+    }
+}
