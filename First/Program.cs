afb8b04a64a48631ad234646363e2eb28632d8e0
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-14 01:33:49 +0200

    First

---
+++ First/Program.cs
@@ -0,0 +1,24 @@
+﻿using LINQ.Shared.V6;
+
+namespace First
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var questions = QuestionBank.All;
+
+            var firstQuestion = questions.First();
+
+            //var someQuestion = questions.First(x => x.Title.Length == 0);   //InvalidOperationException
+            var someQuestion = questions.FirstOrDefault(x => x.Title.Length == 0);
+
+            if (someQuestion is null)
+            {
+                Console.WriteLine("someQuestion is null.\n");
+            }
+
+            Console.WriteLine(firstQuestion);
+        }
+    }
+}
