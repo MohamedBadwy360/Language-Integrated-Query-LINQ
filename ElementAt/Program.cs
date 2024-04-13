d8c341676779c74c7d2c17478883b169664c7bce
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-14 01:23:34 +0200

    ElementAt

---
+++ ElementAt/Program.cs
@@ -0,0 +1,19 @@
+﻿using LINQ.Shared.V6;
+
+namespace ElementAt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var questions = QuestionBank.All;
+
+            var questionAt10 = questions.ElementAt(10);
+            //var questionAt300 = questions.ElementAt(300);     // ArgumentOutOfRangeException
+
+            var questionAt300 = questions.ElementAtOrDefault(300);
+
+            Console.WriteLine(questionAt300);
+        }
+    }
+}
