05b5952a2caeb24fa0b6263bc6a4bd5a9767dc04
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-14 01:57:24 +0200

    Single

---
+++ Single/Program.cs
@@ -0,0 +1,22 @@
+﻿using LINQ.Shared.V6;
+
+namespace Single
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var questions = QuestionBank.All;
+
+            //var question = questions.Single(x => x.Title.Contains("245"));  // InvalidOperationException
+            //var question = questions.SingleOrDefault(x => x.Title.Contains("245"));  // InvalidOperationException
+            //var question = questions.Single(x => x.Title.Length == 0);  // InvalidOperationException
+            var question = questions.SingleOrDefault(x => x.Title.Length == 0); 
+
+            if (question is null)
+            {
+                Console.WriteLine("Question is null.");
+            }
+        }
+    }
+}
