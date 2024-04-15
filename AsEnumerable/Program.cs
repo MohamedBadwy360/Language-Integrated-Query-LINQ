30466d3ba315fa0361e28edb05cdeba1595f6c69
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-15 06:35:19 +0200

    AsEnumerable

---
+++ AsEnumerable/Program.cs
@@ -0,0 +1,18 @@
+﻿using LINQ.Shared.V8;
+
+namespace AsEnumerable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ShippingList<Shipping> shippings = ShippingRepository.AllAsShippingList;
+
+            var todayShippings = shippings.Where(s => s.ShippingDate == DateTime.Today);
+            todayShippings.Process("Today's shippings using ShippingList's Where");
+
+            var todayShippings02 = shippings.AsEnumerable().Where(s => s.ShippingDate == DateTime.Today);
+            todayShippings02.Process("Today's shippings using IEnumerable's Where");
+        }
+    }
+}
