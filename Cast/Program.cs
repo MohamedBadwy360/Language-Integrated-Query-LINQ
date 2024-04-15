0f5cbe67215e6f5b76205a1fafac7d044bcdd8d2
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-15 06:49:12 +0200

    Cast

---
+++ Cast/Program.cs
@@ -0,0 +1,16 @@
+﻿using LINQ.Shared.V8;
+
+namespace Cast
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Shipping> shippings = ShippingRepository.AllAsList;
+
+            var groundShippings = shippings.Where(s => s.GetType() == typeof(GroundShipping)).Cast<GroundShipping>();
+
+            groundShippings.Process("Ground Shippings");
+        }
+    }
+}
