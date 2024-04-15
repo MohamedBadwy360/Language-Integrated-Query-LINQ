1ae9f860de0d717d5bc33a96bd3f51022cece3c4
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-15 06:40:59 +0200

    AsQueryable

---
+++ AsQueryable/Program.cs
@@ -0,0 +1,22 @@
+﻿using LINQ.Shared.V8;
+
+namespace AsQueryable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ShippingList<Shipping> shippings = ShippingRepository.AllAsShippingList;
+
+            var todayShippings = shippings.Where(s => s.ShippingDate == DateTime.Today);
+            todayShippings.Process("Today's shippings using ShippingList's Where");
+            //Console.WriteLine(todayShippings.Expression);     // Not available for IEnumerable
+
+
+            IQueryable<Shipping> todayShippings02 = shippings.AsQueryable().Where(s => s.ShippingDate == DateTime.Today);
+            todayShippings02.Process("Today's shippings using IQueryable<T> Where");
+
+            Console.WriteLine(todayShippings02.Expression);
+        }
+    }
+}
