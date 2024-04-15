739401eab3f96d034c96321f2a136773f0d91961
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-15 06:53:06 +0200

    OfType

---
+++ OfType/Program.cs
@@ -0,0 +1,16 @@
+﻿using LINQ.Shared.V8;
+
+namespace OfType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Shipping> shippings = ShippingRepository.AllAsList;
+
+            var groundShippings = shippings.OfType<GroundShipping>();
+
+            groundShippings.Process("Ground Shippings");
+        }
+    }
+}
