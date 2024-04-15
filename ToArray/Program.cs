159d0eb13be65238bfc07ca36f30edefbc8d608f
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-15 06:58:05 +0200

    ToArray

---
+++ ToArray/Program.cs
@@ -0,0 +1,16 @@
+﻿using LINQ.Shared.V8;
+
+namespace ToArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Shipping> shippings = ShippingRepository.AllAsList;
+            var shippingsArray = shippings.ToArray();
+            Console.WriteLine($"Shippings Count = {shippingsArray.Length}");
+            Console.WriteLine("First Shipping:");
+            shippingsArray[0].Start();
+        }
+    }
+}
