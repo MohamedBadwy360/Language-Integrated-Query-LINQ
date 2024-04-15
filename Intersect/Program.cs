dc66a45e13c9253673e30509d88033754d6b1a2a
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-15 02:06:37 +0200

    Intersect

---
+++ Intersect/Program.cs
@@ -0,0 +1,22 @@
+﻿using LINQ.Shared.V7;
+
+namespace Intersect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var set1 = Repository.Meeting1.Participants;
+            var set2 = Repository.Meeting2.Participants;
+
+            set1.Print($"Meeting 1 Participants {set1.Count}");
+            set2.Print($"Meeting 2 Participants {set2.Count}");
+
+            var set3 = set1.Intersect(set2);
+            set3.Print($"set1.Intersect(set2) {set3.Count()}");
+
+            var set4 = set1.IntersectBy(set2.Select(e => e.EmployeeNo), e => e.EmployeeNo);
+            set4.Print($"set1.IntersectBy(set2.Select(e => e.EmployeeNo), e => e.EmployeeNo) {set4.Count()}");
+        }
+    }
+}
