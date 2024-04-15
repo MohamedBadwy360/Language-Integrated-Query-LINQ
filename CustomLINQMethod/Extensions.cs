1602fd666950cfab438dd11a89fdb12a1427d5b7
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-16 00:02:22 +0200

    CustomLINQMethod

---
+++ CustomLINQMethod/Extensions.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLINQMethod
+{
+    public static class Extensions
+    {
+        private static Random random = new Random();
+
+        public static IEnumerable<TSource> Paginate<TSource>(this IEnumerable<TSource> source, 
+            int page = 1, int pageSize = 10)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException($"{nameof(source)}");
+            }
+
+            if (page <= 0)
+            {
+                //page = 1;
+                throw new ArgumentException($"{nameof(page)}");
+            }
+
+            if (pageSize <= 0)
+            {
+                //pageSize = 10;
+                throw new ArgumentException($"{nameof(pageSize)}");
+            }
+
+            if (!source.Any())
+            {
+                return Enumerable.Empty<TSource>();
+            }
+
+            return source.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+
+        public static IEnumerable<TSource> Paginate2<TSource>(this IEnumerable<TSource> source, 
+            int? page, int? pageSize)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException($"{nameof(source)}");
+            }
+
+            if (!page.HasValue)
+            {
+                page = 1;
+            }
+            
+            if (!pageSize.HasValue)
+            {
+                pageSize = 10;
+            }
+
+            if (page <= 0)
+            {
+                //page = 1;
+                throw new ArgumentException($"{nameof(page)}");
+            }
+
+            if (pageSize <= 0)
+            {
+                //pageSize = 10;
+                throw new ArgumentException($"{nameof(pageSize)}");
+            }
+
+            if (!source.Any())
+            {
+                return Enumerable.Empty<TSource>();
+            }
+
+            return source.Skip((page.Value - 1) * pageSize.Value).Take(pageSize.Value);
+        }
+
+        public static IEnumerable<TSource> WhereWithPaginate<TSource>(this IEnumerable<TSource> source,
+            Func<TSource, bool> predicate,
+            int page, int pageSize)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException($"{nameof(source)}");
+            }
+
+            if (predicate is null)
+            {
+                throw new ArgumentNullException($"{nameof(predicate)}");
+            }
+
+            var result = source.Where(predicate);
+
+            return result.Paginate(page, pageSize);
+        }
+
+        public static TSource Random<TSource>(this IEnumerable<TSource> source, 
+            Func<TSource, bool> predicate)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException($"{nameof(source)}");
+            }
+
+            if (predicate is null)
+            {
+                throw new ArgumentNullException($"{nameof(predicate)}");
+            }
+
+            if (!source.Any())
+            {
+                return default;
+            }
+
+            var result = source.Where(predicate);
+
+            return result.ElementAt(random.Next(0, result.Count()));
+        }
+    }
+}
---
+++ CustomLINQMethod/Program.cs
@@ -0,0 +1,35 @@
+﻿using LINQ.Shared.V9;
+
+namespace CustomLINQMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = Repository.Employees;
+            //employees.Print("All Employees");
+
+            // Pagination
+            
+            //int page = 2;
+            //int pageSize = 30;
+            //var page1 = employees.Skip((page - 1) * pageSize).Take(pageSize);
+
+            var page1 = employees.Paginate();
+            var page2 = employees.Paginate(2);
+            page1.Print("Page #1");
+            page2.Print("Page #2");
+
+            var page3 = employees.Paginate2(3, null);
+            page3.Print("Page #3");
+
+            var page1x7Covered = employees.WhereWithPaginate(e => e.HasHealthInsurance, 1, 7);
+            page1x7Covered.Print("Page #1 [7 Emps Covered]");
+
+            var randomCoveredEmployee = employees.Random(e => e.HasHealthInsurance);
+
+            Console.WriteLine("\nRandom Covered Employee\n");
+            Console.WriteLine(randomCoveredEmployee);
+        }
+    }
+}
