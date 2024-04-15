04c6ea270b43f22e951c70e115e40919ea86d15e
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-04-15 07:23:33 +0200

    ToLookup

---
+++ ToLookup/Program.cs
@@ -0,0 +1,20 @@
+﻿using LINQ.Shared.V8;
+
+namespace ToLookup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Shipping> shippings = ShippingRepository.AllAsList;
+
+            ILookup<string, Shipping> lookup = shippings.ToLookup(s => s.UniqueId);
+            lookup["ABC005"].First().Start();
+
+            ILookup<DateTime, Shipping> lookup02 = shippings.ToLookup(s => s.ShippingDate);
+
+            DateTime date = new DateTime(2024, 4, 15, 0, 0, 0);
+            lookup02[date].Process($"Shippings On Date {date.ToString("dddd, MMMM dd yyyy")}");
+        }
+    }
+}
